7ff6e4012c7d00d5e20b07cf1bfe4fe17e35f660
upendra bajpai <supernovaplazma@gmail.com>
2021-05-31 17:58:48 +0530

    changing config

---
+++ lib/config.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+const APP_NAME="Magento Mobile";
+const BASE_URL_LINK="http://vdsai.com/ecom";
+const ADMIN_TOKEN="Bearer 3f9tp4kppfyjvg9c6qsk6amyd7ymoxkd";
+
+//color pallet
+const PRIMARY_COLOR=Colors.black;
+const PRIMART_LIGHT=Color(0xFFFFECDF);
+const PRIMARY_COLOR_ACCENT=Color(0xFFFFA53E);
+const PRIMARY_GRADIENT_COLOR=Color(0xFFFF7643);
+const SECONDARY_COLOR=Color(0xFF979797);
+const TEXT_COLOR=Color(0xFF757575);
